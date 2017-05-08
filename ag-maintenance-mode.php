3cc707400a39e572b3f543c677d333b850779346
AgilityWeb <mail@agilityweb.co.uk>
2017-05-08 16:25:25 +0100

    Updated function

--- ag-maintenance-mode.php
+++ ag-maintenance-mode.php
@@ -15,7 +15,8 @@
  *
  * Displays the coming soon page for anyone who's not logged in.
  * The login page gets excluded so that you can login if necessary.
- * The Cming Soon Page needs to be constructed in views/maintenance.php
+ *
+ * The Coming Soon Page needs to be constructed in views/maintenance.php
  *
  * @return void
  */
